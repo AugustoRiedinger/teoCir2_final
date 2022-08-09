bebf0e884c99ac640e6055d3e44016e38c237715
AugustoRiedinger <riedinger.augusto@gmail.com>
2022-08-09 14:41:45 -0300

    Added code for octave simulations and removed stm parts

---
+++ code/sim.m
@@ -0,0 +1,9 @@
+pkg load control; pkg load signal;
+clear; clear all; close all;
+
+% - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+% Filtro ellip 6-A
+% - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+% Datos del filtro
+n = 6;
+
