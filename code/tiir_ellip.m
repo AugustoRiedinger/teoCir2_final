1905a20f3ba9e4bf6bd7388539f57394b4f3009a
AugustoRiedinger <riedinger.augusto@gmail.com>
2022-08-10 14:29:55 -0300

    Initialized FFIR ellip filter design

---
+++ code/tiir_ellip.m
@@ -0,0 +1,12 @@
+% Configuracion
+pkg load control; pkg load signal;
+clear; clear all; close all;
+
+% Datos del filtro
+n = 6;
+rp = 0.035;
+rs = 25;
+wp = 0.1;
+
+% Calculo de los coeficientes
+[b,a] = ellip(n,rp,rs,wp)
