4b012e8e99ce5773d822fa85b8008190ec0abdcc
AugustoRiedinger <riedinger.augusto@gmail.com>
2022-08-10 12:55:02 -0300

    Fixed and finished impulse response for tiir hfir+

--- code/tiir.m
+++ code/tiir.m
@@ -10,8 +10,8 @@ q = 0.98;
 N = 300;
 
 % Polinomio de la funcion transferencia HFIR+
-num_HFIR1 = [1 zeros(1,N-1) a -b];
-den_HFIR1 = [1 -p q zeros(1,N-1)];
+num_HFIR1 = [1 zeros(1,N) a -b];
+den_HFIR1 = [1 -p q zeros(1,N)];
 
 % Grafico de la respuesta al impulso
 impz(num_HFIR1,den_HFIR1)
