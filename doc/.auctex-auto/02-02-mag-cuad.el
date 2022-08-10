aeba32dc6e055569d8986d31d41b5c07c3d0cfd0
AugustoRiedinger <riedinger.augusto@gmail.com>
2022-08-10 14:31:34 -0300

    Added initial ellip ffir filter design

--- doc/.auctex-auto/02-02-mag-cuad.el
+++ doc/.auctex-auto/02-02-mag-cuad.el
@@ -1,7 +1,11 @@
 (TeX-add-style-hook
  "02-02-mag-cuad"
  (lambda ()
+   (add-to-list 'LaTeX-verbatim-environments-local "lstlisting")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
    (LaTeX-add-labels
+    "sec:02-02-mag-cuad"
     "eq:57"
     "eq:60"))
  :latex)
--- doc/.auctex-auto/03-00-sim.el
+++ doc/.auctex-auto/03-00-sim.el
@@ -1,10 +1,12 @@
 (TeX-add-style-hook
  "03-00-sim"
  (lambda ()
+   (TeX-run-style-hooks
+    "sections/03-01-ellip")
    (LaTeX-add-labels
     "eq:03-00-hfir+"
     "fig:03-00-hfir+"
-    "eq:03-00-hfir-"
+    "eq:03-00-hfir2"
     "fig:tiir_hfir2"))
  :latex)
 
